035dbade00abf76036cb866304e15c0aaa90c33f
hatahata1648 <89150558+hatahata1648@users.noreply.github.com>
2024-05-22 13:15:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ window.addEventListener('load', function() {
     img.style.opacity = 1; // Fade in the image
 
     setTimeout(function() {
-        window.location.href = 'https://hatahata1648.github.io/seibose/'; // Redirect after total animation time
-    }, 9000); // Total time before redirect: image fade-in (3s) + loading animation (6s)
+        window.location.href = 'https://hatahata1648.github.io/seibose/'; // Redirect after 3 seconds
+    }, 6000); // Total time before redirect: image fade-in (3s) + loading animation (3s)
 });
